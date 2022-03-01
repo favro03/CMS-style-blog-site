f46524aa2fbe6a6870f7d78fff4104a3fd116320
tfavro <wetr9902@gmail.com>
2022-03-01 17:59:38 -0600

    created models

---
+++ models/index.js
@@ -0,0 +1,36 @@
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
+
+// create associations for posts to user
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+//create assication between votes and users
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment};

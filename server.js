7ddace7bde0b3e048ef95f1a9386fcd516023545
tfavro <wetr9902@gmail.com>
2022-03-02 22:06:28 -0600

    added a homepage

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
+const routes = require('./controllers/');
 
 
 const app = express();
